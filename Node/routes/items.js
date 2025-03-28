35cc101ee676183569b447dcf570881494bd2a05
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2025-03-28 17:59:42 +0100

    get /xats from a user update

--- Node/routes/items.js
+++ Node/routes/items.js
@@ -4,16 +4,6 @@ const router = express.Router();
 const Item = require('../models/Item');
 const Xats = require('../models/Xats');
 
-router.get('/xats', async (req, res) => {
-    try {
-      const items = await Xats.find();
-      res.json(items);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  });
-
   router.get('/users', async (req, res) => {
     try {
       const items = await Item.find();
@@ -47,6 +37,57 @@ router.get('/xats', async (req, res) => {
     }
   });
 
-  
+  // Get all friend list and friend request list from a user
+  router.get('/friend-requests', async(req, res) => {
+    const { userId } = req.query;
 
+    // Verify if the userId is present
+    if (!userId) {
+      return res.status(400).send('Bad Request: userId is required');
+    }
+
+    try {
+      // Filter result by userId
+      const user = await Item.findOne({ userId });  
+
+      if (!user) {
+        return res.status(404).send('User not found');
+      }
+
+      //  List of friends & friend_requests from user with userId 
+      const userData = { friends: user.friends, friend_requests: user.Friend_requests };
+      res.json(userData);
+    
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+  // Get all xats from a user
+  router.get('/xats', async (req, res) => {
+    const { userId } = req.query;
+
+    // Verify if the userId is present
+    if (!userId) {
+      return res.status(400).send('Bad Request: userId is required');
+    }
+
+    try {
+      // Filter result by userId
+      const user = await Item.findOne({ userId });  
+
+      if (!user) {
+        return res.status(404).send('User not found');
+      }
+
+      const xats = (user.xats);
+
+      res.json(xats);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
 module.exports = router;
