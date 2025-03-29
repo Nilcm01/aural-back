b5dd545a403a669b9132e37b28347f26ea4d0c90
bernibike <brugar99g@gmail.com>
2025-03-29 13:32:23 +0100

    Added changes made on Personalize-profile branch to make the pull request

--- Node/models/Content.js
+++ Node/models/Content.js
@@ -17,6 +17,10 @@ const ContentSchema = new mongoose.Schema({
     },
     description: {
         type: String,
+    },
+    Comments:{
+        type: Array,
+        default: []
     }
 });
 
--- Node/routes/usersRoutes.js
+++ Node/routes/usersRoutes.js
@@ -1,18 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-const { users, loginUser, profileInfo, friends } = require('../controllers/usersController');
+const { 
+  users, 
+  loginUser, 
+  profileInfo, 
+  friends, 
+  registerUser, 
+  modifyProfile, 
+  sendFriendRequest, 
+  acceptFriendRequest, 
+  removeFriend, 
+  userDescription,
+  userAge
+} = require('../controllers/usersController');
 
-  // Get all users from the DB
-  router.get('/users', users);
+// Get all users from the DB
+router.get('/users', users);
 
-  // Add the user with userId from Spotify API to DB
-  router.post('/login-user', loginUser);
+// Add the user with userId from Spotify API to DB
+router.post('/login-user', loginUser);
 
-  // Get all the information from a user to show at profile screen
-  router.get('/profile-info', profileInfo);
+// Get all the information from a user to show at profile screen
+router.get('/profile-info', profileInfo);
 
-  // Get all friend list and friend request list from a user
-  router.get('/friends', friends);
-  
+// Get all friend list and friend request list from a user
+router.get('/friends', friends);
+
+// Register a new user
+router.post('/register', registerUser); // const { userId, name, username, password, age, imageURL } = req.body;
+
+// Modify user profile
+router.put('/modify-profile', modifyProfile);
+
+// Send a friend request
+router.post('/send-friend-request', sendFriendRequest);
+
+// Accept a friend request
+router.post('/accept-friend-request', acceptFriendRequest);
+
+// Remove a friend
+router.delete('/remove-friend', removeFriend);
+
+// Get user description
+router.put('/user-description', userDescription);
+
+// Get user age
+router.put('/user-age', userAge);
+
 module.exports = router;
